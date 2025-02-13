7e9fadb23bcbb2a11d50f85fd83897db9310cd2e
Umar Musa Halliru <umusa5801@gmail.com>
2025-02-13 14:49:46 +0100

    Renamed student.js to students.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'school'  
+  database: 'school'  // Replace with your database name
 });
 
 db.connect((err) => {
