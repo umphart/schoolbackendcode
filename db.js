7261b553ade8d440cb607425e689ffa03351391d
Umar Musa Halliru <umusa5801@gmail.com>
2025-03-04 16:30:09 +0100

    upd

--- db.js
+++ db.js
@@ -1,42 +1,42 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT || 5432, // Default PostgreSQL port
-//   ssl: {
-//     rejectUnauthorized: false, // Required for Render's PostgreSQL
-//   },
-// });
-
-// pool.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to the database:', err);
-//     process.exit(1);
-//   }
-//   console.log('Database connected successful');
-// });
-
-// module.exports = pool;
-  // db.js
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'school'  // Replace with your database name
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+  ssl: {
+    rejectUnauthorized: false, // Required for Render's PostgreSQL
+  },
 });
 
-db.connect((err) => {
+pool.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
     process.exit(1);
   }
-  console.log('Database connected succefully');
+  console.log('Database connected successful');
 });
 
-module.exports = db;
+module.exports = pool;
+//   // db.js
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'school'  // Replace with your database name
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to the database:', err);
+//     process.exit(1);
+//   }
+//   console.log('Database connected succefully');
+// });
+
+// module.exports = db;
